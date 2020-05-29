9e1d029cd31fdeadd2db84cbb5394879f2071a30
Milan Zinzuvadiya <milanzinzuvadiya99@gmail.com>
2020-05-28 23:58:54 -0400

    add main.py

--- QLearn/main.py
+++ QLearn/main.py
@@ -1,3 +1,14 @@
-from Grid import Grid
+from QGrid import QGrid
+from Reward import Reward
 
-g = Grid(2,4,(0,0),(2,2))
+r = Reward(3,3)
+r2 = Reward(3,3)
+print(r==r2)
+r.setReward(1,1,5)
+r.setReward(1,1,6)
+r2.setReward(1,1,6)
+r2.setReward(1,1,5)
+print(r==r2)
+
+print(r.getRewards())
+print(r2.getRewards())
