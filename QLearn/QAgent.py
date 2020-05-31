ed158694d2f3ed9014c020df3b0c741db63f51fc
Milan Zinzuvadiya <milanzinzuvadiya99@gmail.com>
2020-05-31 01:25:09 -0400

    exploreGoal Method for Agent
    return trajectory as list after reaching goal

--- QLearn/QAgent.py
+++ QLearn/QAgent.py
@@ -10,6 +10,7 @@ class QAgent:
         self.start = start
         self.start_list = start_list
         self.end_states = end_states
+        
     
     def getStart(self):
         if self.start != None:
@@ -24,17 +25,25 @@ class QAgent:
             action = np.random.choice(valid_actions)
         else:
             action = self.qmap.maxQdirection(state)
+        
+        if action not in valid_actions:
+            action = np.random.choice(valid_actions)
+
         return action
 
 
 
 
-
+    #perform one episode and return trajectory
     def exploreGoal(self,exp_rate):
         trajectory = []
         cur_state = self.getStart()
 
         while cur_state not in self.end_states:
             valid_directions = self.qmap.getValidMoves(cur_state)
-            
+            action = self.chooseAction(cur_state,valid_directions,exp_rate)
+            state_action = cur_state[0],cur_state[1],action
+            trajectory.append(state_action)
             cur_state = self.qmap.move(state_action)
+        
+        return trajectory
