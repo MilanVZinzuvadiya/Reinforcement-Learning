42b534db8df6a239c8c89ea019d7230d4e45c837
Milan Zinzuvadiya <milanzinzuvadiya99@gmail.com>
2020-08-10 16:53:23 -0400

    Update main.py
    Changes according to Analysis Class

--- QLearn/main.py
+++ QLearn/main.py
@@ -1,14 +1,51 @@
-from QGrid import QGrid
-from Reward import Reward
+from Analysis import Analysis
+import matplotlib.pyplot as plt
 
-r = Reward(3,3)
-r2 = Reward(3,3)
-print(r==r2)
-r.setReward(1,1,5)
-r.setReward(1,1,6)
-r2.setReward(1,1,6)
-r2.setReward(1,1,5)
-print(r==r2)
+rows = 3
+cols = 10
+block = [(1,1),(1,3),(1,5),(1,6)]
+reward = [ ((0,9),2.0), ((1,9),-2.0) ]
+start_states = [(2,0)]
+end_states = [ (0,9),(1,9) ]
 
-print(r.getRewards())
-print(r2.getRewards())
+constAnalysis = Analysis(rows,cols,block,reward,start_states,end_states)
+adaptiveAnalysis = Analysis(rows,cols,block,reward,start_states,end_states)
+
+episodesConst,matConst,constLength = constAnalysis.trainConst(1.0,0.5,0.5)
+episodesAdap,matAdap,adapLength,gammas = adaptiveAnalysis.trainAdaptive(1.0,0.5,0.5)
+
+
+print('Static Gamma')
+print('episodes : ',episodesConst)
+print('Total Trajectory Length : ',constLength)
+print('Adaptive Gamma')
+print('episodes : ',episodesAdap)
+print('Total Trajectory Length : ',adapLength)
+print('Num of Gamma change: ',len(gammas))
+print('Gammas : ',gammas)
+print('========================================================================')
+episodes = [episodesConst,episodesAdap]
+lengths = [constLength,adapLength]
+
+
+
+
+
+
+
+
+
+
+
+
+x = [0,1]
+#episodesConst = episodesConst*25
+#episodesAdap = int(0.857*episodesConst)
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Episodes and changing Gamma')
+ax2.plot(x, episodes)
+ax1.bar(x,lengths)
+ax1.set_ylabel('Total Trajectory Length')
+ax1.set_xticks(x,('Constant Gamma','Adaptive Gamma'))
+plt.show()
